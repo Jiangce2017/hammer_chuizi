12c76ce79a11d7b24b0f0c9db21d7ef0334a2e20
mebaldwi <mebaldwi@andrew.cmu.edu>
2023-03-21 11:51:23 -0400

    Plotting code modified for plotting voxels

--- src/hammer/utilities/plotting.py
+++ src/hammer/utilities/plotting.py
@@ -4,6 +4,8 @@ import imageio
 import numpy as np
 import os.path as osp
 import trimesh
+import pickle
+
 
 def plot_surf_mesh(tris):
     # Create a new plot
@@ -97,11 +99,181 @@ def plot_toolpath_with_voxels(toolpath, voxels):
                 marker = 'o')
     
     plt.show()
-    
-def plot_test_voxel(voxel_interpolation):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    cmap = plt.get_cmap("gray")
-    ax.voxels(voxel_interpolation, edgecolor="k", facecolors=cmap(voxel_interpolation))
-    plt.show()
-        
+
+
+########################################################################################################################
+# TESTING
+import plotly.graph_objects as go
+from plotly_voxel_display.VoxelData import VoxelData
+from matplotlib import cm
+import matplotlib
+
+
+def plotly_fig(subsample, title):
+    # subsample: 3D array
+
+    # Establish the 3D Plot axes (modified for non-cubic windows)
+    x_axis = np.shape(subsample)[0]
+    y_axis = np.shape(subsample)[1]
+    z_axis = np.shape(subsample)[2]
+    X, Y, Z = np.mgrid[0:1:(x_axis*1j), 0:1:(y_axis*1j), 0:1:(z_axis*1j)]
+
+    # Determine aspect ratio of plot
+    minimum = min(x_axis, y_axis, z_axis)
+    x_ratio = x_axis/minimum
+    y_ratio = y_axis/minimum
+    z_ratio = z_axis/minimum
+
+    # Establish the Maximum Value
+    maximum = np.amax(subsample)
+
+    # Write the Figure
+    fig = go.Figure(data=go.Volume(
+        x=X.flatten(),
+        y=Y.flatten(),
+        z=Z.flatten(),
+        value=subsample.flatten(),
+        isomin=1,  # This establishes the minimum value that is displayed
+        isomax=maximum,
+        opacity=0.1,  # Needs to be small to see through all surfaces
+        surface_count=100, # Larger values mean more surfaces to define the shape
+        # slices_z=dict(show=True, locations=[0.4]), # add slice to better show color variation in top view
+        colorscale='Bluered',  # Possible color values: 'haline' #Blackbody,Bluered,Blues,Cividis,Earth,Electric,Greens,
+                               # Greys,Hot,Jet,Picnic,Portl and,Rainbow,RdBu,Reds,Viridis,YlGnBu,YlOrRd.
+        ))
+    fig.update_scenes(aspectmode='manual', aspectratio_x=x_ratio, aspectratio_y=y_ratio, aspectratio_z=z_ratio,
+                      xaxis_showticklabels=False, yaxis_showticklabels=False, zaxis_showticklabels=False)
+    fig.update_layout(
+        scene={
+            # 'zaxis': {'autorange': 'reversed'} # reverse automatically)
+            },
+        title=title
+        )
+    fig.show()
+    return fig
+
+
+def mesh_3D_voxels(arr, title):
+    Voxels = VoxelData(arr)
+
+    fig = go.Figure(data=go.Mesh3d(
+            x=Voxels.vertices[0],
+            y=Voxels.vertices[1],
+            z=Voxels.vertices[2],
+            i=Voxels.triangles[0],
+            j=Voxels.triangles[1],
+            k=Voxels.triangles[2],
+            intensity=arr.flatten(),
+            colorscale='Bluered',
+            opacity=0.75,
+            # isomin=1,  # This establishes the minimum value that is displayed
+            # isomax=2000,
+            ))
+    fig.update_layout(
+            # scene={
+            #     'zaxis': {'autorange': 'reversed'} # reverse automatically)
+            #     },
+            scene=dict(aspectmode='data'),
+            title=title
+            )
+    fig.show()
+    return fig
+
+
+def matplot_voxels(arr, color_bar_label, title=None, minimum=None, maximum=None, cmap='bwr',subplot=None, fig=None, fontsize=12):
+    if subplot == None:
+        # Create a new figure
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+    else:
+        ax = fig.add_subplot(*subplot, projection='3d')
+    #
+
+    # Plot the voxels
+    cmap = plt.get_cmap(cmap)
+
+    if minimum == None:
+        minimum = np.amin(arr)
+    if maximum == None:
+        maximum = np.amax(arr)
+
+    norm = matplotlib.colors.Normalize(vmin=minimum, vmax=maximum)
+    m = cm.ScalarMappable(cmap=cmap, norm=norm)
+    m.set_array([])
+
+    cax = fig.add_axes([ax.get_position().x1 + 0.01, ax.get_position().y0, 0.02, ax.get_position().height])
+    cbar = plt.colorbar(m, cax=cax, aspect=0.5)
+    cbar.ax.tick_params(labelsize=fontsize/2)
+    cbar.set_label(color_bar_label, fontsize=fontsize/2)
+    plt.ticklabel_format(style="plain")
+    ax.voxels(arr, edgecolor="k", facecolors=cmap(norm(arr)), alpha=0.5)
+    # ax.invert_zaxis()
+
+    # Display the plot
+    if title != None:
+        ax.set_title(title, fontsize=fontsize)
+
+    ax.tick_params(axis='x', labelsize=fontsize/2)
+    ax.tick_params(axis='y', labelsize=fontsize / 2)
+    ax.tick_params(axis='z', labelsize=fontsize / 2)
+    return ax, fig
+
+########################################################################################################################
+def plot_data(true, prediction, windows, time_steps, sample_indices, file_name, fig, fontsize, worst_error=False):
+    count = 0
+    columns = 4
+    number_samples = len(sample_indices)
+    if worst_error:
+        num_samples = true.shape[0]
+        true_u = true.reshape(num_samples, -1)
+        pred_u = prediction.reshape(num_samples, -1)
+        mse = np.mean((pred_u - true_u) ** 2, axis=1)
+
+        worst_sample_indices = np.argsort(mse)[-number_samples:]
+        sample_indices = worst_sample_indices
+
+
+
+    for row, index in enumerate(sample_indices):
+
+        # activation_sample = activation[index]
+        # a_sample = a[index]
+        u_pred_sample = prediction[index]
+        u_true_sample = true[index]
+        window_sample = windows[index]
+        time_step_sample = time_steps[index]
+
+        # List titles
+        if row == 0:  # Only plot the titles for the first row
+            title1 = "True"
+            title2 = "Predicted"
+            title3 = "Difference"
+            title4 = "Percent Error"
+        else:
+            title1 = title2 = title3 = title4 = None
+        # Plot the True Sample
+        count += 1
+        ax1, fig1 = matplot_voxels(u_true_sample, "Temperature",title=title1, subplot=(number_samples, columns, count), fig=fig,
+                                   fontsize=fontsize)
+        ax1.text(-18, -18, 4,
+                 "Sample: " + str(index) + "\nTime step: " + str(time_step_sample) + "\nWindow: " + str(window_sample),
+                 verticalalignment='center', fontsize=fontsize)
+
+        # Plot the predicted sample
+        count += 1
+        ax2, fig2 = matplot_voxels(u_pred_sample, "Temperature",title=title2, subplot=(number_samples, columns, count), fig=fig,
+                                   fontsize=fontsize)
+
+        # Plot Difference
+        error = u_true_sample - u_pred_sample
+        count += 1
+        ax3, fig3 = matplot_voxels(error, "Temperature", title=title3, subplot=(number_samples, columns, count), fig=fig, fontsize=fontsize)
+
+        # Plot Percent Error
+        # Calculate the percentage errors, handling the case where actual is 0
+        u_error = np.where(u_true_sample == 0, 0, np.divide(abs(error), abs(u_true_sample)) * 100)
+        count += 1
+        ax4, fig4 = matplot_voxels(u_error, "% Error", title=title4, subplot=(number_samples, columns, count), fig=fig,
+                                   fontsize=fontsize, cmap='Reds')
+
+    plt.savefig("figures/" + file_name)
